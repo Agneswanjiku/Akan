4dbedb3be1525c1242a43d46185fa416c636778b
edithntinyari <emothegi@gmail.com>
2020-05-05 16:46:56 +0300

    add changes on script.js

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ var femaleNames = ["Akosua", "Adoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 
 //user interface logic
-function findNames(){
+function findNames(e){
+    e.preventDefault();
+    // alert("e");
     var day = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
